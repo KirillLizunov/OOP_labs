df908a1354f20169e9dd562b28ae7476a8d38f1a
Kirillita <kirill89828@gmail.com>
2024-12-19 21:54:24 +0300

    update sol.cpp

--- lab1/src/sol.cpp
+++ lab1/src/sol.cpp
@@ -1,24 +1,17 @@
-#include "sol.h"  
+#include <iostream>
+#include <string>
 
-std::string replacer(int n, char old_Value, char new_Value,  std::string text) {
-    unsigned int counter {0};
-    unsigned int count_symbol {0};
-
-    for (int size_t = 0; size_t < text.size(); ++size_t){
-        count_symbol++;
-    }
-    if (n <= 0 || n > count_symbol){
-        return text;
-        }
-    for (int i = 0; i < text.size(); ++i) {  
-        if (text[i] == old_Value) {
-            counter ++;
-        }
-        if (counter == n) {
-            text[i] = new_Value;
-            counter = 0;
+std::string replacer(int n, char old_Value, char new_Value, std::string text) {
+    int count = 0;
+    for (char &c : text) {
+        if (c == old_Value) {
+            if (count < n) {
+                c = new_Value;
+                count++;
+            } else {
+                break;
+            }
         }
     }
-
     return text;
 }
